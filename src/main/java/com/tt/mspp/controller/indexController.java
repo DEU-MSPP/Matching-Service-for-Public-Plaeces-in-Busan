660d88506b04cdf8db58c6fc9d1e99e8cb7c1953
Hyeonsu <gustn05233@naver.com>
2023-05-19 18:17:55 +0900

    공공장소 지도 마킹 기능 구현 1

--- src/main/java/com/tt/mspp/controller/indexController.java
+++ src/main/java/com/tt/mspp/controller/indexController.java
@@ -4,6 +4,10 @@ import com.tt.mspp.dao.DAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.tt.mspp.dto.PlaceDTO;
+import org.springframework.ui.Model;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +15,8 @@ import java.util.List;
 @Controller
 public class indexController {
 
-    @GetMapping("/index")
-    public String index() {
+    @GetMapping("/")
+    public String index(Model model) {
         DAO dao = DAO.getInstance();
         List<PlaceDTO> placelist = dao.getPlaceList();
         String index = "";
@@ -22,19 +26,27 @@ public class indexController {
         int type = 0;
         String phone = "";
 
-        //이중배열
-        //ArrayList<Integer> list = new ArrayList<String>();
-
+        String[] markerTitles = new String[placelist.size()];
+        String[] markerAddresses = new String[placelist.size()];
+        int[] markerType = new int[placelist.size()];
         for (int i = 0; i < placelist.size(); i++) {
-            index = placelist.get(i).getP_index();
-            address = placelist.get(i).getP_address();
-            name = placelist.get(i).getP_name();
-            reserve = placelist.get(i).getP_reserve();
-            type = placelist.get(i).getP_type();
-            phone = placelist.get(i).getP_phone();
-
+            markerTitles[i] = placelist.get(i).getP_name();
+            markerAddresses[i] = placelist.get(i).getP_address();
+            markerType[i] = placelist.get(i).getP_type();
+            //index = placelist.get(i).getP_index();
+            //address = placelist.get(i).getP_address();
+            //name = placelist.get(i).getP_name();
+            //reserve = placelist.get(i).getP_reserve();
+            //type = placelist.get(i).getP_type();
+            //phone = placelist.get(i).getP_phone();
         }
+        model.addAttribute("markerTitles", markerTitles);
+        model.addAttribute("markerAddresses", markerAddresses);
+        model.addAttribute("markerType", markerType);
 
         return "index";
     }
+
 }
+
+
